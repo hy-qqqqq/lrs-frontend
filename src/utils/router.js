3920c129798b2d449bb439c1d98b128951abe4c6
hy-qqqqq <amy.s.perng@gmail.com>
2024-05-06 19:56:35 +0800

    Feat: add approval route & names

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,13 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Auth from '../components/Auth.vue'
 import Order from '../components/Order.vue'
+import Approval from '../components/Approval.vue'
 import Dashboard from '../components/Dashboard.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path: '/', component: Auth},
-    {path: '/order', component: Order},
-    {path: '/dashboard', component: Dashboard},
-  ]
+    {path: '/', name: 'auth', component: Auth},
+    {path: '/order', name: 'order', component: Order},
+    {path: '/approval', name: 'approval', component: Approval},
+    {path: '/dashboard', name: 'dashboard', component: Dashboard},
+  ],
+  linkActiveClass: 'underline underline-offset-4'
 });
