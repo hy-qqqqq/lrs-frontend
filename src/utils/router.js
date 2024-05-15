c905c5f524e7505fbe7d8f6cd6ee90adb26fc95a
ashuantw <trista1217@gmail.com>
2024-05-15 12:43:24 +0800

    Feat: add router

--- src/utils/router.js
+++ src/utils/router.js
@@ -3,11 +3,15 @@ import Auth from '../components/Auth.vue'
 import Order from '../components/Order.vue'
 import Approval from '../components/Approval.vue'
 import Dashboard from '../components/Dashboard.vue'
+import Login from '../components/auths/Login.vue'
+import Register from '../components/auths/Register.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     {path: '/', name: 'auth', component: Auth},
+    {path: '/login', name: 'longin', component: Login},
+    {path: '/register', name: 'register', component: Register},
     {path: '/order', name: 'order', component: Order},
     {path: '/approval', name: 'approval', component: Approval},
     {path: '/dashboard', name: 'dashboard', component: Dashboard},
