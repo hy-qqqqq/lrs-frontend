4684080a6bbe6b1679bdd52fcbf412c81bb32154
hy-qqqqq <amy.s.perng@gmail.com>
2024-05-17 22:34:16 +0800

    Chore: add vuetify

--- src/main.js
+++ src/main.js
@@ -5,12 +5,29 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { router } from './utils/router.js' 
 import { filter } from './utils/filter.js' 
-import App from './App.vue'
 
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+const vuetify = createVuetify({
+  components,
+  directives,
+  treeShake: true,
+  icons: {
+    iconfont: 'mdi',
+  }
+})
+
+// App
+import App from './App.vue'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 app.config.globalProperties.$filters = filter;
 
 app.mount('#app')
