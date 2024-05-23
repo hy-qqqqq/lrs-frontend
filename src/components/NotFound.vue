1aa0204f5984e426dd6a7a044d7f0d24ae7da580
hy-qqqqq <amy.s.perng@gmail.com>
2024-05-24 00:12:58 +0800

    Fix: add 404 component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col justify-center items-center gap-5 p-8">
+    <h2 >404 Not found. The page you are looking for does not exist.</h2>
+    <img src="../assets/404.gif" loop="0" width="125" height="125">
+    <!-- <a href="https://www.flaticon.com/free-animated-icons/error" title="error animated icons">Error animated icons created by Freepik - Flaticon</a> -->
+  </div>
+</template>
